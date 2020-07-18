56aa65945130a324f9ffe4344559d3f90c47771c
miguelrodriguez8 <67935472+miguelrodriguez8@users.noreply.github.com>
2020-07-18 11:54:43 -0700

    FSDI 102 Class 2 homework

---
+++ script.js
@@ -0,0 +1,40 @@
+console.log("Loaded");
+
+// var let const
+
+var name; // declaration of the variable
+name="Samantha"; //assigntation of the variable
+
+console.log(name);
+
+var studentName="John"; //camelCase
+console.log(studentName);
+
+
+
+student = "Colin";
+console.log(student);
+
+const taxes=0.16;
+console.log(taxes); //could be declared and assigned just once.
+
+console.log(2+2);
+console.log(5-3);
+console.log(4*2);
+console.log(5/2);
+
+var number="100";
+console.log(number);
+
+//console.log(Number(number));
+
+//string functions and operators
+console.log(name.toUpperCase());
+console.log(studentName.toLowerCase());
+let msg;
+
+msg= name + " is the instructor, and " + student + " is the student. ";
+console.log(msg);
+
+
+
---
+++ script2.js
@@ -0,0 +1,39 @@
+// challenge : save in a variable the information that you ger from the prompt and display it on the console.
+
+// var info = prompt("Enter the info");
+//console.log(info)
+
+
+
+var name = prompt("Enter the product's name");
+console.log(name)
+
+var price = prompt("Enter the price");
+console.log(price);
+
+var quantity = prompt("Enter the quantity");
+console.log(quantity);
+
+const taxes=0.16
+
+var subtotal=price*quantity;
+console.log("The subtotal is " + subtotal);
+
+var tax=subtotal*0.16;
+console.log("Your tax will be " + tax);
+
+var total=subtotal+tax;
+console.log("Your total will be " + total);
+
+
+
+
+
+
+
+
+//console.log(quantity + )
+
+//total price * quantity
+
+//subtotal var = info
